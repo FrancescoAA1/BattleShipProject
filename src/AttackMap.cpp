2807fca400408137b7147cdc9ab50c80281f6566
mattiagalassi02 <mattiagalassi02@gmail.com>
2022-12-29 16:27:47 +0100

    Aggiunta operatori di position

--- src/AttackMap.cpp
+++ src/AttackMap.cpp
@@ -1,1 +1,14 @@
 #include "../include/AttackMap.h"
+
+// costruttore di default che setta tutto a vuoto
+AttackMap::AttackMap()
+{
+    // imposto tutte le celle 
+    for (int i = 0; i < kHeight; i++)
+    {
+        for (int j = 0; j < kWidth; j++)
+        {
+            attack_map_[i][j] = AttackUnit::empty; 
+        }
+    }
+}
