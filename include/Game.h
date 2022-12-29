44f41150d7b9dcd4547cb242fcbd76945b04ccad
FrancescoAA1 <francescofantin2002@gmail.com>
2022-12-29 23:14:27 +0100

    aggiunta commenti

--- include/Game.h
+++ include/Game.h
@@ -11,18 +11,24 @@ class Game
 {
     public:
 
+    //costruttore che accetta i nomi dei due giocatori, la modalità di gioco e il numero di round
     Game(const std::string& nickname_1, const std::string& nickname_2, GameMode mode, int nRounds);
 
+    //funzione che 
     void playRound();
 
+    //funzione che attesta se uno dei due giocatori ha vinto la partita
     bool Win();
 
     private:
 
+    //puntatori ai due player
     Player* player_1;
     Player* player_2;
 
+    //numero di turni 
     int numberOfRounds;
+    //modalità di gioco
     GameMode mode;
 
 };
