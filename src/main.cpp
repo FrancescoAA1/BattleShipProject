0300b8af32a8ec6fb2e7042e45d2b8f5b5754828
FrancescoAA1 <francescofantin2002@gmail.com>
2023-01-02 20:00:36 +0100

    main.cpp comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
 int main()
 {
-    return 0;
+    //1) Controllo del numero di parametri
+    //Se sono più di due, si tratta di una funzionalità di replay (da implementare)
+    //Se sono due ossia "./main" e "pc" (o "cc")
+    //2)Creazione di un oggetto di classe Game (vanno forniti due nickname,
+    // modalità giocata, numero di turni)
+    //2.1) Scelta casuale del giocatore che inizia
+    //3) Scelta delle navi --> viene invocato il metodo add(Player& p)
+    //4) FIntanto che uno dei due giocatori non vince continua a giocare round
 }
